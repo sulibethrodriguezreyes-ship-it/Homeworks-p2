3ab9a34d14ff19606e4a1bda2fe45ba1b2bb0444
sulibethrodriguezreyes-ship-it <sulibethrodriguezreyes@gmail.com>
2026-02-10 00:06:50 -0400

    adding the first homework

---
+++ first homework1/ExStudent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace first_homework
+{
+    public class ExStudent : Person
+    {
+        public int Id { get; set; }
+        public string Career { get; set; }
+        public int GraduationYear { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CurrentJob { get; set; }
+
+    }
+}
---
+++ first homework1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace first_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
