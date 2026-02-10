bef0053fcd88c201c469484205d3501be2c678d9
sulibethrodriguezreyes-ship-it <sulibethrodriguezreyes@gmail.com>
2026-02-09 23:43:38 -0400

    First-homework
    Adding the first homework

---
+++ Administrator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace first_homework
+{
+    public class Administrator : Person
+    {
+        public int Id { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+        public DateTime HireDate { get; set; }
+        public int YearsExperience { get; set; }
+        public string ContractType { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ Commynitymember.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace first_homework
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int birthday { get; set; }
+        public int  gender { get; set; }
+
+    }
+}
