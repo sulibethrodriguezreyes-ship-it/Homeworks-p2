8375bf8d15af559ddb0d64f3dfbafe67b728b40d
sulibethrodriguezreyes-ship-it <sulibethrodriguezreyes@gmail.com>
2026-02-14 21:10:46 -0400

    Revert "uploading my second homework"

--- RefugioSocialAPI/DTOs/StaysCreateDTo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RefugioSocialAPI.DTOs
-{
-    public class StaysCreateDTo
-    {
-        public DateTime CheckInDate { get; set; }
-        public DateTime CheckOutDate { get; set; }
-        public int PersonId { get; set; }
-    }
-}
